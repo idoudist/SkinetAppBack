46f16ddaa023243c47c1b4cb9b196ff397a10514
Fadi Idoudi <fadi.idoudi@outlook.com>
2025-12-27 22:49:51 +0100

    feat(api): add filter and sort endpoints

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,9 +9,9 @@ namespace API.Controllers;
 public class ProductsController(IProductRepository repo) : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
     {
-        return Ok(await repo.GetProductsAsync());
+        return Ok(await repo.GetProductsAsync(brand, type, sort));
     }
 
     [HttpGet("{id:int}")]
@@ -60,6 +60,18 @@ public class ProductsController(IProductRepository repo) : ControllerBase
 
         await repo.SaveChangesAsync();
         return NoContent();
+    }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        return Ok(await repo.GetBrandsAsync());
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+    {
+        return Ok(await repo.GetTypesAsync());
     }
 
     private bool ProductExist(int id)
