3f71bc1e49feb03e7fc9293b0d1628957d1f30da
Fadi Idoudi <fadi.idoudi@outlook.com>
2025-12-30 01:12:47 +0100

    feat(Data): adding specification pattern

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -6,6 +6,8 @@ public interface IGenericRepository<T> where T : BaseEntity
 {
     Task<T?> GetByIdAsync(int id);
     Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T?> GetEntityWithSpecAsync(ISpecification<T> spec);
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
     void Add(T entity);
     void Update(T entity);
     void Delete(T entity);
