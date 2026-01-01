5fc02c2a4156589960038d0e5a44032610fff577
Fadi Idoudi <fadi.idoudi@outlook.com>
2026-01-01 14:52:56 +0100

    feat(Middleware): add exception handeling middleware

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware(IHostEnvironment env, RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex, env);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception ex, IHostEnvironment env)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        var response = env.IsDevelopment()
+            ? new Errors.ApiErrorResponse(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+            : new Errors.ApiErrorResponse(context.Response.StatusCode, ex.Message, "Internal Server Error");
+
+        JsonSerializerOptions options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        string json = JsonSerializer.Serialize(response, options);
+
+        return context.Response.WriteAsync(json);
+    }
+}
