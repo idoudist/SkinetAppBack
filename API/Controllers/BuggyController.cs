439032cfe06ca108bb2ef7a9b618ce471eb52a56
Fadi Idoudi <fadi.idoudi@outlook.com>
2026-01-01 14:34:26 +0100

    feat(test): adding buggy controller

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    [HttpGet("notfound")]
+    public IActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+    [HttpGet("internalerror")]
+    public IActionResult GetServerError()
+    {
+        throw new Exception("This is a server error");
+    }
+    [HttpGet("badrequest")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("This is a bad request");
+    }
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+    [HttpPost("validationerror")]
+    public IActionResult GetValidationError(Product product)
+    {
+        return Ok();
+    }
+}
